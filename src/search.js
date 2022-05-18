75e17e0d40be4c26c712d00faded3c295fc393d4
chrisgth <ksemkus00@gmail.com>
2022-05-19 00:40:37 +0300

    fixed search function

--- src/search.js
+++ src/search.js
@@ -3,8 +3,10 @@ export default function search() {
     const searchbtn = document.getElementById('search')
     const input = document.getElementById('location')
 
-    searchbtn.addEventListener('click', () => {
-       let data = dataprocessor(input.value)
-       console.log(data)
+    let procSearchData = 0
+
+    searchbtn.addEventListener('click', async () => {
+        procSearchData = await dataprocessor(input.value)
+        console.log(procSearchData)
     })
 }
