30e464b200ef02a1e9fc774cb1ffe2ba3c2daf91
chrisgth <ksemkus00@gmail.com>
2022-05-19 02:15:19 +0300

    Utilized search function to display a default city

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import { dataprocessor } from "./dataprocessor";
+import displayData from "./displayData";
 import { fetchapi } from "./fetchapi";
 import search from "./search";
 import './style.css'
 
-search()
+search(true)
--- src/search.js
+++ src/search.js
@@ -1,6 +1,16 @@
 import { dataprocessor } from "./dataprocessor"
 import displayData from "./displayData"
-export default function search() {
+export default function search(pageloader) {
+
+    if(pageloader) {
+        async function init() {
+            const pageLoadLocation = await dataprocessor('Vilnius')
+            displayData(pageLoadLocation)
+            console.log(pageLoadLocation)
+        }
+        init();
+    }
+
     const searchbtn = document.getElementById('search')
     const input = document.getElementById('location')
 
