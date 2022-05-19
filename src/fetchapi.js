90afc276b8a1c5126249bf1ad4bd9dbc5f901d56
chrisgth <ksemkus00@gmail.com>
2022-05-19 03:37:18 +0300

    Fix security issue

--- src/fetchapi.js
+++ src/fetchapi.js
@@ -1,5 +1,5 @@
 export const fetchapi = async (location) => {
-    const getdata = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=ae493baf78620325326ab4c420c1674a&units=metric`, {mode: 'cors'})
+    const getdata = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=ae493baf78620325326ab4c420c1674a&units=metric`, {mode: 'cors'})
     if (getdata.status !== 200) {
         throw new Error('Error fetching api')
     }
